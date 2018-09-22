34fc1743afa3c25e1e6822109fbb802cf5680a05
bertnp <bertnp@users.noreply.github.com>
2018-09-22 09:17:27 -0400

    Make output for clean repos optional

--- gitstatus.py
+++ gitstatus.py
@@ -9,7 +9,11 @@ import re
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("dir", help="Directory to search for repositories.",
+parser.add_argument('--show-clean',
+    help='Show status for clean, up-to-date repositories',
+    action='store_true')
+parser.add_argument('dir',
+    help='Directory to search for repositories.',
     nargs='?', default=os.getcwd())
 args = parser.parse_args()
 
@@ -30,7 +34,7 @@ for root,dirs,files in os.walk(args.dir):
         no_changes = False
 
         repo_name = os.path.basename(os.path.normpath(root))
-        print(col.PURPLE + repo_name + col.ENDC)
+        repo_name = col.PURPLE + repo_name + col.ENDC
 
         result = sp.run(['git', 'status'], cwd=root, stdout=sp.PIPE)
         output = result.stdout.decode('utf-8')
@@ -40,13 +44,16 @@ for root,dirs,files in os.walk(args.dir):
         if pattern_nochanges.search(output):
             no_changes = True
 
-        if up_to_date and no_changes:
-            print(col.GREEN + "Up to date with remote. Nothing to commit.\n" +
+        if up_to_date and no_changes and args.show_clean:
+            print(repo_name)
+            print(col.GREEN + 'Up to date with remote. Nothing to commit.\n' +
                 col.ENDC)
             continue
         if not no_changes:
-            print(col.RED + "Changed since last commit.\n" + col.ENDC)
+            print(repo_name)
+            print(col.RED + 'Changed since last commit.\n' + col.ENDC)
             continue
         if not up_to_date:
-            print(col.YELLOW + "Has changes to push to remote.\n" + col.ENDC)
+            print(repo_name)
+            print(col.YELLOW + 'Has changes to push to remote.\n' + col.ENDC)
             continue
