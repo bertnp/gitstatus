3bc3bf057c60f32fd3214a045e2c3d088eb6b85b
bertnp <bertnp@users.noreply.github.com>
2018-09-25 07:03:46 -0400

    Add option to git fetch in each repo

--- gitstatus.py
+++ gitstatus.py
@@ -12,10 +12,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--show-clean',
     help='Show status for clean, up-to-date repositories',
     action='store_true')
+parser.add_argument('--fetch',
+    help='Run git fetch in each repository',
+    action='store_true')
 parser.add_argument('dir',
     help='Directory to search for repositories.',
     nargs='?', default=os.getcwd())
-args = parser.parse_args()
+args,unknown = parser.parse_known_args()
 
 class col:
     RED = '\033[0;31m'
@@ -36,6 +39,11 @@ for root,dirs,files in os.walk(args.dir):
         repo_name = os.path.basename(os.path.normpath(root))
         repo_name = col.PURPLE + repo_name + col.ENDC
 
+        if args.fetch:
+            print(repo_name)
+            sp.run(['git', 'fetch'], cwd=root)
+            continue
+
         result = sp.run(['git', 'status'], cwd=root, stdout=sp.PIPE)
         output = result.stdout.decode('utf-8')
 
@@ -55,5 +63,5 @@ for root,dirs,files in os.walk(args.dir):
             continue
         if not up_to_date:
             print(repo_name)
-            print(col.YELLOW + 'Has changes to push to remote.\n' + col.ENDC)
+            print(col.YELLOW + 'Out of sync with remote.\n' + col.ENDC)
             continue
